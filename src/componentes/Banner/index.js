d18ce587cbc2847524f7bdfe18b58bbcd9898baf
Jessica <jessicagama9827@gmail.com>
2024-12-16 12:09:54 -0300

    Organo finalizado

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,14 @@
+import './Banner.css'
+
+const Banner = () => {
+    // JSX
+    return (
+        <>
+            <header className="banner">
+                <img src="/imagens/banner.png" alt="O banner principal da página do organo" />
+            </header>
+        </>
+    )
+}
+
+export default Banner
